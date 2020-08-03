bbd0cd1f6f7ef81a87c1ed04637757395a5e784e
Petros <petrospetrosyanjs.96@gmail.com>
2020-08-04 03:18:52 +0400

    Updates

---
+++ precache-manifest.7ea07dc5a5f0a95f7dabe9e7f9d35a66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c226a945f5f00840b2bad9f1cf25c656",
+    "url": "/type_racer/index.html"
+  },
+  {
+    "revision": "2d4e05874274862e56a1",
+    "url": "/type_racer/static/css/main.3682c3bd.chunk.css"
+  },
+  {
+    "revision": "2c14efca3e44ae83bf8e",
+    "url": "/type_racer/static/js/2.ba5e66b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/type_racer/static/js/2.ba5e66b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4e05874274862e56a1",
+    "url": "/type_racer/static/js/main.0e65bdd0.chunk.js"
+  },
+  {
+    "revision": "c939c6d3e7411bd6b0d7",
+    "url": "/type_racer/static/js/runtime-main.57dfed66.js"
+  },
+  {
+    "revision": "3986a10054c67954ab797b0d8885e2f9",
+    "url": "/type_racer/static/media/keyboard.3986a100.webp"
+  }
+]);
